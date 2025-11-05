39e475c1b770016723bf515c6fdde4d3f335dfc9
Angeline Aurel Efendy <angelineaurel@Angelines-MacBook-Pro.local>
2025-11-05 22:24:47 +0000

    add: munge/02_exploration script for exploration

--- munge/02_exploration.R
+++ munge/02_exploration.R
@@ -59,3 +59,79 @@ BLOOD_PRESSURE %>%
     color = "Sex"
   )
 
+#-------------------------------------------------------------------------
+
+#Average OBESITY BMI AGE : COUNTRY
+BMI_AGE %>%
+  group_by(location) %>%
+  summarise(mean_obesity = mean(BMI_mean, na.rm = TRUE)) %>%
+  arrange(desc(mean_obesity)) %>%
+  ggplot(aes(x = reorder(location, mean_obesity), y = mean_obesity)) +
+  geom_col(fill = "steelblue") +
+  coord_flip() +
+  labs(
+    title = "Average Obesity Prevalence by Country",
+    x = "Country",
+    y = "Mean Obesity Prevalence (%)"
+  )
+
+
+#Average OBESITY BMI AGE : SEX
+BMI_AGE %>%
+  group_by(sex) %>%
+  summarise(mean_obesity = mean(BMI_mean, na.rm = TRUE)) %>%
+  ggplot(aes(x = sex, y = mean_obesity, fill = sex)) +
+  geom_col() +
+  labs(
+    title = "Average Obesity Prevalence by Sex",
+    x = "Sex",
+    y = "Mean Obesity Prevalence (%)"
+  )
+
+
+#Average OBESITY BMI AGE : YEAR
+BMI_AGE %>%
+  group_by(year) %>%
+  summarise(mean_obesity = mean(BMI_mean, na.rm = TRUE)) %>%
+  ggplot(aes(x = year, y = mean_obesity)) +
+  geom_line(color = "darkorange", size = 1) +
+  geom_point(color = "red") +
+  labs(
+    title = "Global Trend of Obesity Over Time",
+    x = "Year",
+    y = "Mean Obesity Prevalence (%)"
+  )
+
+
+#Average OBESITY BMI AGE : YEAR & SEX (2 varable)
+BMI_AGE %>%
+  group_by(year, sex) %>%
+  summarise(mean_obesity = mean(BMI_mean, na.rm = TRUE)) %>%
+  ggplot(aes(x = year, y = mean_obesity, color = sex)) +
+  geom_line(size = 1) +
+  geom_point() +
+  labs(
+    title = "Obesity Trend by Sex Over Time",
+    x = "Year",
+    y = "Mean Obesity Prevalence (%)",
+    color = "Sex"
+  )
+
+
+#Countries with the Largest Changes in Obesity
+BMI_change <- BMI_AGE %>%
+  group_by(location) %>%
+  summarise(
+    first_year = min(year),
+    last_year = max(year),
+    obesity_start = BMI_mean[year == first_year],
+    obesity_end = BMI_mean[year == last_year],
+    change = obesity_end - obesity_start
+  ) %>%
+  arrange(desc(change))
+head(BMI_change, 10)
+
+#---------------------------------------------------------------------
+
+
+  
